d591cf4a42d9c390063fcd22e7ccae4b5f884831
Shinya Ihara <129726604+Shinya-GitHub-Center@users.noreply.github.com>
2024-09-17 14:57:04 +0900

    small change

--- aws-cli/workdir/switch-role.sh
+++ aws-cli/workdir/switch-role.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# Please execute shellscript file under source command
-# If you use bash command, environment variables can not be set...
+# Load variables
+source ./vars-switchrole.txt
 
-# Getting variables from external txt file
-. ./vars-switchrole.txt
-
-# getting credentials - key id, access key, and token
+# Get authentication credentials
 AWS_STS_CREDENTIALS=$(aws sts assume-role \
---role-arn arn:aws:iam::"${TARGET_ACCTID}":role/"${ROLE_NAME}" \
---role-session-name "${SESSION_NAME}")
+  --role-arn arn:aws:iam::"${TARGET_ACCTID}":role/"${ROLE_NAME}" \
+  --role-session-name "${SESSION_NAME}")
 
-# extract the each credential using jq command
+# Extract credentials
 AWS_ACCESS_KEY_ID=$(echo "${AWS_STS_CREDENTIALS}" | jq -r '.Credentials.AccessKeyId')
 AWS_SECRET_ACCESS_KEY=$(echo "${AWS_STS_CREDENTIALS}" | jq -r '.Credentials.SecretAccessKey')
 AWS_SESSION_TOKEN=$(echo "${AWS_STS_CREDENTIALS}" | jq -r '.Credentials.SessionToken')
 
-# setting issued credentials into environment variables
+# Set environment variables
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN
+
+echo "Switched role successfully."
